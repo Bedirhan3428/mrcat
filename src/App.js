19667e01f6a3ff81d25b653038240521f13e4564
Bedirhan <abimer2350@gmail.com>
2025-03-28 16:05:26 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
     <div className="App">
       <Routes>
       <Route path='/' element={<Signup />}/>
-      <Route path='/singup' element={<Home />}/>
+      <Route path='/home' element={<Home />}/>
       </Routes>
       <div>
       {/* Uygulamanızın diğer bileşenleri */}
